74aa0abdf55848b22b318755d74a733ae3c6c7bd
bundy huang <bundy.huang@myob.com>
2024-01-03 12:33:55 +1100

    listings

--- Escort-api/Escort.Event.Domain/Models/EventDetails.cs
+++ Escort-api/Escort.Event.Domain/Models/EventDetails.cs
@@ -2,7 +2,7 @@ namespace Escort.Event.Domain.Models;
 
 public class EventDetails : BaseDomainModel
 {
-    public EventDetails(DateTime date, DateTime startTime, DateTime endTime, DateTime bookingTime, User.Domain.Models.User user, Client.Domain.Models.Client client, Driver.Domain.Models.Driver? driver)
+    public EventDetails(DateTime date, DateTime startTime, DateTime endTime, DateTime bookingTime, User.Domain.Models.User user, Client.Domain.Models.Client client, Driver.Domain.Models.Driver? driver, Listing.Domain.Models.Listing listing)
     {
         Date = date;
         StartTime = startTime;
@@ -11,7 +11,9 @@ public class EventDetails : BaseDomainModel
         User = user;
         Client = client;
         Driver = driver;
+        Listing = listing;
     }
+
 
     public DateTime Date { get; set; }
     public DateTime StartTime { get; set; }
@@ -20,6 +22,7 @@ public class EventDetails : BaseDomainModel
     public User.Domain.Models.User User { get; set; }
     public Client.Domain.Models.Client Client { get; set; }
     public Driver.Domain.Models.Driver? Driver { get; set; } 
+    public Listing.Domain.Models.Listing Listing { get; set; }
     
  
     public EventDetails WithId()
--- Escort-api/Escort.Listing.Domain/Models/Listing.cs
+++ Escort-api/Escort.Listing.Domain/Models/Listing.cs
@@ -1,6 +1,17 @@
 namespace Escort.Listing.Domain.Models;
 
-public class Listing: BaseDomainModel
+public class Listing : BaseDomainModel
 {
-    
+    public required bool Listed { get; set; }
+    public required User.Domain.Models.User ListingOwner;
+
+    public Listing(User.Domain.Models.User user)
+    {
+        ListingOwner = user;
+    }
+    public Listing WithId()
+    {
+        this.Id = Guid.NewGuid();
+        return this;
+    }
 }
