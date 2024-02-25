c84c3de9a0d236f933d7e37a6408f0c89c8edb4f
bundy huang <bundy.huang@myob.com>
2024-02-25 19:42:01 +1100

    fixing some models

--- Escort-api/Escort.Client.Domain/Models/ClientContactDetails.cs
+++ Escort-api/Escort.Client.Domain/Models/ClientContactDetails.cs
@@ -1,9 +1,23 @@
 namespace Escort.Client.Domain.Models;
 
-public class ClientContactDetails
+public class ClientContactDetails: BaseDomainModel
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
+    
+    public ClientContactDetails(string firstName, string lastName, string email, string phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+    
+    public ClientContactDetails WithId()
+    {
+        this.Id = Guid.NewGuid();
+        return this;
+    }
 }
--- Escort-api/Escort.Driver.Domain/Models/DriverContactDetails.cs
+++ Escort-api/Escort.Driver.Domain/Models/DriverContactDetails.cs
@@ -1,9 +1,23 @@
 namespace Escort.Driver.Domain.Models;
 
-public class DriverContactDetails
+public class DriverContactDetails : BaseDomainModel
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
+    
+    public DriverContactDetails(string firstName, string lastName, string email, string phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+    
+    public DriverContactDetails WithId()
+    {
+        this.Id = Guid.NewGuid();
+        return this;
+    }
 }
