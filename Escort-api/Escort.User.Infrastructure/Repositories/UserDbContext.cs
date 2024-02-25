59a3f18309238aa9e9a28e56b62855a9e5eb64d0
bundy huang <bundy.huang@myob.com>
2024-02-25 19:29:58 +1100

    db context for user

---
+++ Escort-api/Escort.User.Infrastructure/Repositories/UserDbContext.cs
@@ -0,0 +1,27 @@
+using Escort.User.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escort.User.Infrastructure.Repositories;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+    {}
+    
+    public DbSet<Domain.Models.User> Users { get; set; }
+    public DbSet<UserContactDetails> UserContactDetails { get; set; }
+    public DbSet<UserVerificationDetails> UserVerificationDetails { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Models.User>()
+            .HasOne(u => u.UserContactDetails)
+            .WithOne()
+            .HasForeignKey<UserContactDetails>(ucd => ucd.Id);
+
+        modelBuilder.Entity<Domain.Models.User>()
+            .HasOne(u => u.UserVerificationDetails)
+            .WithOne()
+            .HasForeignKey<UserVerificationDetails>(uvd => uvd.Id);
+    }
+}
