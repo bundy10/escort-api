8319da37c4fc294db30f382c4684f53b3cab51a5
bundy huang <bundy.huang@myob.com>
2024-03-18 15:12:35 +1100

    get user with listings

--- Escort-api/Escort.User.Infrastructure/Repositories/UserRepository.cs
+++ Escort-api/Escort.User.Infrastructure/Repositories/UserRepository.cs
@@ -9,4 +9,20 @@ public class UserRepository : BaseRepository<Domain.Models.User>, IUserRepositor
     public UserRepository(DbContext context) : base(context)
     {
     }
+    public async Task<Domain.Models.User> GetUserWithListingsAsync(Guid userId)
+    {
+        var user = await _entities.Include(u => u.Listings).SingleOrDefaultAsync(u => u.Id == userId);
+
+        if (user == null)
+        {
+            throw new Exception("User not found");
+        }
+
+        if (user.Listings == null || !user.Listings.Any())
+        {
+            throw new Exception("User has no listings");
+        }
+
+        return user;
+    }
 }
