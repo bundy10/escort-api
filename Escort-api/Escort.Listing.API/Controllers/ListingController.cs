3208689855d2ab4582aa683cb1304058aeb8ecd4
bundy huang <bundy.huang@myob.com>
2024-03-18 16:09:08 +1100

    refactor adding listing collection to user controller and dtos

--- Escort-api/Escort.Listing.API/Controllers/ListingController.cs
+++ Escort-api/Escort.Listing.API/Controllers/ListingController.cs
@@ -1,9 +1,59 @@
+using Escort.Listing.API.DTO;
+using Escort.Listing.Application.Repositories;
+using Escort.Listing.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Escort.Listing.API.Controllers;
 
+[ApiController]
+[Route("api/[controller]")]
 public class ListingController : Controller
 {
+    private readonly IListingRepository _listingRepository;
     
+    public ListingController(IListingRepository listingRepository)
+    {
+        _listingRepository = listingRepository;
+    }
     
+    [HttpGet]
+    public async Task<IActionResult> GetAllListings()
+    {
+        var listings = await _listingRepository.GetAllAsync();
+        return Ok(listings.Select(listing => listing.ToDto()));
+    }
+    
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetListingById(Guid id)
+    {
+        var listing = await _listingRepository.GetByIdAsync(id);
+        return Ok(listing.ToDto());
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult<IEnumerable<ListingGetDTO>>> CreateListing(ListingPostPutDto listingPostPutDto)
+    {
+        var listing = await _listingRepository.CreateAsync(listingPostPutDto.ToDomain());
+        return CreatedAtAction(nameof(GetListingById), new { id = listing.Id }, listing.ToDto());
+    }
+    
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult<ListingGetDTO>> UpdateListing(Guid id, [FromBody] ListingPostPutDto listingPostPutDto)
+    {
+        {
+            try
+            {
+                var listing = listingPostPutDto.ToDomain();
+                listing.Id = id;
+                await _listingRepository.UpdateAsync(listing);
+                var listingGetDto = listing.ToDto();
+
+                return Ok(listingGetDto);
+            }
+            catch (ModelNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
 }
--- Escort-api/Escort.Listing.API/DTO/ListingPostPutDto.cs
+++ Escort-api/Escort.Listing.API/DTO/ListingPostPutDto.cs
@@ -1,6 +1,10 @@
+using Escort.Listing.Domain.Models;
+
 namespace Escort.Listing.API.DTO;
 
 public class ListingPostPutDto
 {
-    
+    public bool Listed { get; set; }
+    public ListingDetails ListingDetails { get; set; }
+    public Guid UserId { get; set; }
 }
