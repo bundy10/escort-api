6d65819c0b185f141dd6c982f9d859461ef1dea1
bundy huang <bundy.huang@myob.com>
2024-03-18 15:13:36 +1100

    adding

--- Escort-api/Escort.User.Application/Repositories/IUserRepository.cs
+++ Escort-api/Escort.User.Application/Repositories/IUserRepository.cs
@@ -2,5 +2,5 @@ namespace Escort.User.Application.Repositories;
 
 public interface IUserRepository : IRepository<Domain.Models.User>
 {
-    
+    Task<Domain.Models.User> GetUserWithListingsAsync(Guid userId);
 }
