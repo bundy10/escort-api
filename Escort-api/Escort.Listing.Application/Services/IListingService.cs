60f739a2fd4e9cd00fb26afac0a6e85b0ff6ccb0
bundy huang <bundy.huang@myob.com>
2024-03-18 15:13:46 +1100

    refactor

--- Escort-api/Escort.Listing.Application/Services/IListingService.cs
+++ Escort-api/Escort.Listing.Application/Services/IListingService.cs
@@ -4,7 +4,7 @@ public interface IListingService
 {
     Task<IEnumerable<Domain.Models.Listing>> GetAllAsync();
     Task<Domain.Models.Listing> GetByIdAsync(Guid id);
-    Task<Domain.Models.Listing> CreateAsync(Domain.Models.ListingDetails listingDetails);
-    Task<Domain.Models.Listing> UpdateAsync(Domain.Models.ListingDetails listingDetails);
+    Task<Domain.Models.Listing> CreateAsync(Domain.Models.ListingDetails listingDetails, Guid userId);
+    Task<Domain.Models.Listing> UpdateAsync(Domain.Models.ListingDetails listingDetails, Guid userId);
     Task<Domain.Models.Listing> DeleteAsync(Guid id);
 }
