0f5953eb9733938831e7746f929713fe7a6d738f
Josh <jthalappillil7@gmail.com>
2024-10-20 03:51:04 +1100

    migrations and addition of password and username to user model

--- Escort-api/src/services/user/Escort.User.Domain/Models/User.cs
+++ Escort-api/src/services/user/Escort.User.Domain/Models/User.cs
@@ -6,6 +6,12 @@ public class User : BaseDomainModel
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
+    
+    public required string Password { get; set; }
+    
+    public required string UserName { get; set; }
     public required UserContactDetails UserContactDetails { get; set; }
     public UserVerificationDetails? UserVerificationDetails {get; set;}
+    
+    
 }
---
+++ Escort-api/src/services/user/Escort.User.Infrastructure/Migrations/20241019161323_ADD-USERNAME-AND-PASSWORD.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Escort.User.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADDUSERNAMEANDPASSWORD : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "Users");
+        }
+    }
+}
