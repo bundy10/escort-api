d14b99b964b174a8baa20f83fcb4045a2fa6002a
Bundy.Huang <bundyhuang1999@gmail.com>
2024-10-20 06:27:12 +1100

    temp user and pass

--- Escort-api/src/services/user/Escort.User.Domain/Models/BaseDomainModel.cs
+++ Escort-api/src/services/user/Escort.User.Domain/Models/BaseDomainModel.cs
@@ -2,6 +2,8 @@ namespace Escort.User.Domain.Models;
 
 public class BaseDomainModel
 {
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
     public int Id { get; set; }  
 }
 
