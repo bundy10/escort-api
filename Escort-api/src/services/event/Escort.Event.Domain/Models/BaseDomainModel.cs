f41185c1e085ba75aa3d60494a9c5b69c2767ef3
Bundy.Huang <bundyhuang1999@gmail.com>
2024-12-02 03:15:06 +1100

    added more properties to event base

--- Escort-api/src/services/event/Escort.Event.Domain/Models/BaseDomainModel.cs
+++ Escort-api/src/services/event/Escort.Event.Domain/Models/BaseDomainModel.cs
@@ -3,4 +3,8 @@ namespace Escort.Event.Domain.Models;
 public class BaseDomainModel
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ClientId { get; set; }
+    public int? DriverId { get; set; } 
+    public int ListingId { get; set; }
 }
