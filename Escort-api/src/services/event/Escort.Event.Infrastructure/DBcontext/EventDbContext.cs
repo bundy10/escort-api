6633e8fdad2d051495744dccd201e8c139418eb0
Bundy.Huang <bundyhuang1999@gmail.com>
2024-12-02 03:16:11 +1100

    Fixing Events data and adding endpoint to get by user id

--- Escort-api/src/services/event/Escort.Event.Infrastructure/DBcontext/EventDbContext.cs
+++ Escort-api/src/services/event/Escort.Event.Infrastructure/DBcontext/EventDbContext.cs
@@ -8,13 +8,4 @@ public class EventDbContext : DbContext
     {
     }
     public DbSet<Domain.Models.Event> Events { get; set; }
-    public DbSet<Domain.Models.EventDetails> EventDetails { get; set; }
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Domain.Models.Event>()
-            .HasOne(e => e.EventDetails)
-            .WithOne()
-            .HasForeignKey<Domain.Models.EventDetails>(ed => ed.Id);
-    }
 }
