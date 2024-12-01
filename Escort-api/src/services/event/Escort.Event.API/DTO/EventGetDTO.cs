716f27096d0c6ea8063d157f8d880a57adbeed20
Bundy.Huang <bundyhuang1999@gmail.com>
2024-12-02 03:16:26 +1100

    Fixing Events data and adding endpoint to get by user id

--- Escort-api/src/services/event/Escort.Event.API/DTO/EventGetDTO.cs
+++ Escort-api/src/services/event/Escort.Event.API/DTO/EventGetDTO.cs
@@ -3,4 +3,10 @@ namespace Escort.Event.API.DTO;
 public class EventGetDTO : EventPostPutDto
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
+    public int ClientId { get; set; }
+    public int? DriverId { get; set; }
+    
+    public int ListingId { get; set; }
+    
 }
