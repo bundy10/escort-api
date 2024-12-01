f639fee74e2463afaba9ffeb34b06e2815567133
Bundy.Huang <bundyhuang1999@gmail.com>
2024-12-02 03:15:58 +1100

    Fixing Events data and adding endpoint to get by user id

--- Escort-api/src/services/event/Escort.Event.Domain/Models/Event.cs
+++ Escort-api/src/services/event/Escort.Event.Domain/Models/Event.cs
@@ -2,6 +2,9 @@ namespace Escort.Event.Domain.Models;
 
 public class Event : BaseDomainModel
 {
-    public EventDetails EventDetails { get; set; }
-    public bool Completed { get; set; }
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public DateTime BookingTime { get; set; }
+    public string Status { get; set; } = "Pending";
 }
